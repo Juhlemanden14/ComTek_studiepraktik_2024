7abdaea6560d7acd81d9e54ccd692283c31e4835
Juhlemanden14 <marcusjuhlandersson@gmail.com>
2024-10-09 07:32:59 +0200

    set up skeleton

---
+++ server_code/server_socket.py
@@ -0,0 +1,9 @@
+"""
+Dette script indeholder et eksempel på at oprette et socket (åbne en port, så computeren kan kommunikere derfra).
+Måden det virker på:
+ - opret server socket      (sæt en postkasse op)
+ - lyt på socket            (lad postmanden komme med post)
+ - accepter forbindelser    (--||--)
+ - modtag beskeder/pakker   (læs posten, som postmanden kommer med)
+"""
+
